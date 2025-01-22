a27b62b0b010edc5535cff9e92bd4188c435fdd2
Oskar André <93657491+oskarandre@users.noreply.github.com>
2025-01-22 23:52:10 +0100

    remove github pages config

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+  // output: "export",  // <=== enables static exports
+  // distDir: "dist",  // <=== changes the build directory
+  // reactStrictMode: true,
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
