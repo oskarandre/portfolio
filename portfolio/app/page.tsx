5bd8853eb7448712a84c62d9c93ed0fd35f30698
Oskar André <93657491+oskarandre@users.noreply.github.com>
2025-01-28 21:27:22 +0100

    added buttons

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -87,6 +87,13 @@ const ART_SLIDES = [
     description: "",
     link: ""
   },
+
+  {
+    image: "/art/iron.png",
+    title: "Iron",
+    description: "",
+    link: ""
+  },
   {
     image: "/art/strand.png",
     title: "strand",
@@ -110,7 +117,7 @@ export default function Home() {
 
   useEffect(() => {
     const handleScroll = () => {
-      const sections = ["home", "projects", "bio"];
+      const sections = ["home", "projects", "art", "bio"];
       const scrollPosition = window.scrollY + window.innerHeight / 2;
 
       for (const section of sections) {
@@ -144,11 +151,22 @@ export default function Home() {
       <div className="flex flex-col "> 
     
       <section id="home" className="min-h-screen flex flex-col items-center justify-center p-8 sm:p-0">
-        <h2 className="text-4xl">Hello, I&apos;m Oskar.</h2>
+        <h2 className="text-4xl">Hello, I&apos;m <span className="text-[var(--secondary-color)]">Oskar</span>.</h2>
         <p className="text-center max-w-2xl">
           Welcome to my portfolio. Here you can find information about my projects and skills.
         </p>
+
+        <div className="mt-8">
+          <button onClick={() => handleClick("projects")} className="bg-[var(--secondary-color)] text-white px-4 py-2 rounded-lg">View Projects</button>
+        </div>
+
+        <div className="mt-8">
+          <svg className="w-6 h-6 animate-bounce" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+          </svg>
+        </div>
       </section>
+      
 
       <section id="projects" className="min-h-screen flex flex-col items-center justify-center">
         <div className="flex flex-col items-center justify-center p-8 sm:p-0">
@@ -156,15 +174,35 @@ export default function Home() {
         </div>
         
         <EmblaCarousel slides={SLIDES} options={OPTIONS}/>
+
+        <div className="mt-8">
+          <button onClick={() => handleClick("art")} className="bg-[var(--secondary-color)] text-white px-4 py-2 rounded-lg">Art</button>
+        </div>
+
+        <div className="mt-8">
+          <svg className="w-6 h-6 animate-bounce" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+          </svg>
+        </div>
       </section>
 
       <section id="art" className="min-h-screen flex flex-col items-center justify-center p-8 sm:p-0">
         <h2 className="text-4xl">Art</h2>
         
         <EmblaCarouselArt slides={ART_SLIDES} options={OPTIONS}/>
+
+        <div className="mt-8">
+          <button onClick={() => handleClick("bio")} className="bg-[var(--secondary-color)] text-white px-4 py-2 rounded-lg">Bio</button>
+        </div>
+
+        <div className="mt-8">
+          <svg className="w-6 h-6 animate-bounce" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+          </svg>
+        </div>
       </section>
 
-      <section id="bio" className="min-h-screen flex flex-col items-center justify-center p-8 sm:p-0">
+      <section className="min-h-screen flex flex-col items-center justify-center p-8 sm:p-0">
         <h2 className="text-4xl">BIO</h2>
         <p className="text-center max-w-2xl">
         I am a 5th-year student pursuing an M.Sc. in Media Technology and Engineering at Linköping University, Sweden. 
@@ -173,7 +211,7 @@ export default function Home() {
         {/* Add contact details here */}
       </section>
 
-      <footer className="p-4 bg-[var(--header-color)] text-white text-center w-full">
+      <footer id="bio" className="p-4 bg-[var(--header-color)] text-white text-center w-full">
           <div className="mt-2">
             <a href="https://www.linkedin.com/in/oskarandre/" target="_blank">
               <Image src="/linkedin.svg" alt="LinkedIn" className="inline-block w-10 mr-5" width={100} height={100} />
